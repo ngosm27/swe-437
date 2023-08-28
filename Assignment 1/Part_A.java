ebab5a8e2ab57b6a4792130ff014af4326e86622
ngosm27 <ngo.susan.m@gmail.com>
2023-08-28 08:09:15 -0400

    formatted assignment 1, part a

--- Assignment 1/Part_A.java
+++ Assignment 1/Part_A.java
@@ -1,41 +1,41 @@
 import java.util.Vector;
 
-public class Part1a{
+public class Part_A {
 
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Vector<Integer> a = new Vector<>();
         a.add(1);
         a.add(2);
         a.add(3);
         a.add(5);
         a.add(2);
-        
+
         Vector<Integer> b = new Vector<>();
         b.add(4);
         b.add(2);
 
         Vector<Integer> res = union(a, b);
-        for(Integer x : res){
+        for (Integer x : res) {
             System.out.print(x + ", ");
         }
     }
-	
-    public static Vector union(Vector a, Vector b){
+
+    public static Vector union(Vector a, Vector b) {
         Vector res = new Vector();
 
         // copy all elements in vector 'a' into vector 'res'; ignore dupes
-        for(int i = 0; i < a.size(); i++){
-            if(!res.contains(a.elementAt(i))){
+        for (int i = 0; i < a.size(); i++) {
+            if (!res.contains(a.elementAt(i))) {
                 res.add(a.elementAt(i));
             }
         }
         // copy all elements in vector 'b' into vector 'res'; ignore dupes
-        for(int i = 0; i < b.size(); i++){
-            if(!res.contains(b.elementAt(i))){
+        for (int i = 0; i < b.size(); i++) {
+            if (!res.contains(b.elementAt(i))) {
                 res.add(b.elementAt(i));
             }
         }
 
         return res;
-	}
+    }
 }
