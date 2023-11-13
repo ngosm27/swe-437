6e4ba7cd790ea5a3a703c3008ce0b73b9f2d9813
Rohina <rohinanaderi@gmail.com>
2023-11-13 01:32:06 -0500

    Wrote mock servlet tests

--- Assignment 9/src/test/java/org/example/conversionTest.java
+++ Assignment 9/src/test/java/org/example/conversionTest.java
@@ -34,18 +34,134 @@ public class conversionTest {
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-    }
 
-    @AfterEach
-    void tearDown() {
+        /*Step 1: Set up for the mock response writer*/
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
     }
 
     @Test
     void doPostTest1() throws ServletException, IOException {
 
+        /*Step 2: Set all the parameters as 100, as per our first test input, then check if they match our expected output*/
+
+        when(request.getParameter("F")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("37.78", stringWriter.toString().trim());
+
+        when(request.getParameter("C")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("212.0", stringWriter.toString().trim());
+
+        when(request.getParameter("in")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("254.0", stringWriter.toString().trim());
+
+        when(request.getParameter("cm")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("39.37", stringWriter.toString().trim());
+
+        when(request.getParameter("ft")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("30.48", stringWriter.toString().trim());
+
+        when(request.getParameter("m")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("328.084", stringWriter.toString().trim());
+
+        when(request.getParameter("mi")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("160.9", stringWriter.toString().trim());
+
+        when(request.getParameter("km")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("62.14", stringWriter.toString().trim());
+
+        when(request.getParameter("gal")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("378.5", stringWriter.toString().trim());
+
+        when(request.getParameter("L")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("26.42", stringWriter.toString().trim());
+
+        when(request.getParameter("oz")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("2835.0", stringWriter.toString().trim());
+
+        when(request.getParameter("g")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("3.53", stringWriter.toString().trim());
+
+        when(request.getParameter("lb")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("45.36", stringWriter.toString().trim());
+
+        when(request.getParameter("kg")).thenReturn("100");
+        servlet.doPost(request, reponse);
+        assertequals("220.5", stringWriter.toString().trim());
     }
 
     @Test
     void doPostTest2() throws ServletException, IOException {
+        /*Step 2: Set all the parameters as 100, as per our first test input, then check if they match our expected output*/
+
+        when(request.getParameter("F")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("C")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("in")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("cm")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("ft")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("m")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("mi")).thenReturn("");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("km")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("gal")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("L")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("oz")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("g")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("lb")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
+
+        when(request.getParameter("kg")).thenReturn("0");
+        servlet.doPost(request, reponse);
+        assertequals("", stringWriter.toString().trim());
     }
 }
